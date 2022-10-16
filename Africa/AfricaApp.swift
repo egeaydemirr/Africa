2cce8afe3753d11abfcd6a0869340fe691989261
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-16 22:16:54 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Ege Aydemir on 16.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
