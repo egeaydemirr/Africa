60061540a37998724693155b59398fb93ee62902
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-17 21:07:03 +0300

    Extension for JSON

--- Africa/Extension/CodableBundleExtension.swift
+++ Africa/Extension/CodableBundleExtension.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+extension Bundle {
+    
+    func decode(_ file: String) -> [CoverImage] {
+        // 1. Locate the Json File
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle")
+        }
+    
+    
+    
+    // 2. Create a property for the data
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+    // 3. Crate a decoder
+    let decoder = JSONDecoder()
+    // 4. Create a property for the decoder data
+        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+            fatalError("Failed the decode \(file) from bundle.")
+        }
+    // 5. Return the ready-to-use data
+        return loaded 
+    }
+}
