ec34cd0087bd0537abebab3812e62071faff0eff
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-17 19:54:56 +0300

    Code Preps and Filing

---
+++ Africa/App/GalleryView.swift
@@ -0,0 +1,20 @@
+//
+//  GalleryView.swift
+//  Africa
+//
+//  Created by Ege Aydemir on 17.10.2022.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text("Gallery")
+    }
+}
+
+struct GalleryView_Previews: PreviewProvider {
+    static var previews: some View {
+        GalleryView()
+    }
+}
---
+++ Africa/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Ege Aydemir on 17.10.2022.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
