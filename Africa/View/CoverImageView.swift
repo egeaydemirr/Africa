4431a22539d22aa745045443e53247970c8b3347
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-17 20:12:14 +0300

    CoverImageView and TabView

---
+++ Africa/View/CoverImageView.swift
@@ -0,0 +1,32 @@
+//
+//  CoverImageView.swift
+//  Africa
+//
+//  Created by Ege Aydemir on 17.10.2022.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    //MARK: PROPERTIES
+    
+    //MARK: BODY
+    var body: some View {
+        TabView {
+            ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                Image("cover-lion")
+                    .resizable()
+                .scaledToFit()
+            }//: LOOP
+        }//:TABVIEW
+        .tabViewStyle(PageTabViewStyle())
+        
+    }
+}
+//MARK: PREVIEW
+struct CoverImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
+    }
+}
