38dcfd5b2737c60b9765c8a0559f0722a3bc9543
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-18 17:30:18 +0300

    Shuffle

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct VideoListView: View {
     //MARK: PROPERTIES
-    var videos: [Video] = Bundle.main.decode("videos.json")
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
     //MARK: BODY
     var body: some View {
         NavigationView {
@@ -19,6 +19,16 @@ struct VideoListView: View {
             }
             .listStyle(InsetGroupedListStyle())
             .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        //action
+                        videos.shuffle()
+                    }) {
+                        Image(systemName:"arrow.2.squarepath")
+                    }
+                }
+            }
         }//: Navigation
     }
 }
