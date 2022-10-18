80c33ca89cf9abd04ffcc484d66c31c099e00376
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-18 17:07:37 +0300

    Video List

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct VideoListView: View {
+    //MARK: PROPERTIES
+    var videos: [Video] = Bundle.main.decode("videos.json")
+    //MARK: BODY
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List(videos) { item in
+                VideoListItem(video: item)
+            }
+        }//: Navigation
     }
 }
-
+//MARK: PREVIEW
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
