7be464625e21b6ff290a96cd61140cb30a8ae2c9
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-18 16:39:55 +0300

    Data Model Video and Video List Item

---
+++ Africa/Model/VideoModel.swift
@@ -0,0 +1,8 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by Ege Aydemir on 18.10.2022.
+//
+
+import Foundation
---
+++ Africa/View/VideoListItem.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListItem.swift
+//  Africa
+//
+//  Created by Ege Aydemir on 18.10.2022.
+//
+
+import SwiftUI
+
+struct VideoListItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VideoListItem_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListItem()
+    }
+}
