39ba0a0d8a1ea4c2804067c7e81f083242557bb0
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-18 11:21:48 +0300

    List Item

---
+++ Africa/View/AnimalListItemView.swift
@@ -0,0 +1,43 @@
+//
+//  SwiftUIView.swift
+//  Africa
+//
+//  Created by Ege Aydemir on 18.10.2022.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        HStack(alignment: .center, spacing: 16) {
+            Image("lion")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 90, height: 90)
+                .clipShape(
+                    RoundedRectangle(cornerRadius: 12)
+                )
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Lion")
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                Text("The world's most social felines, lions roam the savannas and grasslands of the African continent, hunting cooperatively and raising cubs in prides.")
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+                    .padding(.trailing, 8)
+                    
+            }//:VSTACK
+        }//: HSTACK
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
