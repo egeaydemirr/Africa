8acc23da460b02435b2665632eab25098f5d9a01
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-20 15:27:46 +0300

    Effect

--- Africa/View/MotionAnimationView.swift
+++ Africa/View/MotionAnimationView.swift
@@ -11,6 +11,28 @@ struct MotionAnimationView: View {
     //MARK: PROPERTIES
     
     @State private var randomCircle = Int.random(in: 12...16)
+    @State private var isAnimating: Bool = false
+    //MARK: FUNCTIONS
+    //1: RONDOM COORDINATE
+    func randomCoordinate(max: CGFloat) -> CGFloat {
+        return CGFloat.random(in: 0...max)
+    }
+    //2: RANDOM SIZE
+    func randomSize() -> CGFloat {
+        return CGFloat(Int.random(in: 10...300))
+    }
+    //3: RANDOM SCALE
+    func randomScale() -> CGFloat {
+        return CGFloat(Double.random(in: 0.1...2.0))
+    }
+    //4: RANDOM SPEED
+    func randomSpeed() -> Double {
+        return Double.random(in: 0.25...1.0)
+    }
+    //5: RANDOM DELAY
+    func randomDelay() -> Double {
+        return Double.random(in: 0...2)
+    }
     
     //MARK: BODY
     var body: some View {
@@ -20,13 +42,25 @@ struct MotionAnimationView: View {
                     Circle()
                         .foregroundColor(.gray)
                         .opacity(0.15)
-                        .frame(width: 256, height: 256, alignment: .center)
+                        .frame(width: randomSize(), height: randomSize(), alignment: .center)
+                        .scaleEffect(isAnimating ? randomScale(): 1)
                         .position(
-                            x:geometry.size.width/2,
-                            y: geometry.size.height/2
+                            x:randomCoordinate(max: geometry.size.width),
+                            y: randomCoordinate(max: geometry.size.height)
                     )
+                        .animation(
+                            Animation.interpolatingSpring(stiffness: 0.5, damping: 0.5)
+                                .repeatForever()
+                                .speed(randomSpeed())
+                                .delay(randomDelay()),
+                            value: isAnimating
+                        )
+                        .onAppear(
+                            perform: {
+                                isAnimating = true
+                            }
+                        )
                 }//:LOOP
-                Text("Width: \(Int(geometry.size.width)) Height: \(Int(geometry.size.height))")
             }//:ZSTACK
         }//:GEOMETRY
     }
