630a80bebff1c17af29b1c2e2f21dfcc27820e9f
Ege Aydemir <egeaydemirr97@gmail.com>
2022-10-20 17:21:05 +0300

    Photo Gallery

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct GalleryView: View {
     //MARK: PROPERTIES
+    @State private var selectedAnimal: String = "lion"
+    
     let animals: [Animal] = Bundle.main.decode("animals.json")
     //SIMPLE GRID DEFINITION
 //    let gridLayout: [GridItem] = [
@@ -25,15 +27,29 @@ struct GalleryView: View {
         ScrollView(.vertical, showsIndicators: false) {
             //MARK: GRID
             
-            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
-                ForEach(animals) { item in
-                    Image(item.image)
-                        .resizable()
-                        .scaledToFit()
-                        .clipShape(Circle())
-                        .overlay(Circle().stroke(Color.white, lineWidth: 1))
-                }//:LOOP
-            }//:Grid
+            VStack(alignment: .center, spacing: 30) {
+                //MARK: IMAGE
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                            }
+                    }//:LOOP
+                }//:Grid
+            }//:VSTACK
+            .padding(.vertical, 50)
+            .padding(.horizontal, 10)
         }//:SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
